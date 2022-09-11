9d95163c3caa9751d3dab35930102afaac7ecf3f
AEZakmee <kristiyan99@protonmail.com>
2022-09-11 20:03:51 +0300

    Change to gesture detector

--- lib/screens/vote_screen/components/parties_row.dart
+++ lib/screens/vote_screen/components/parties_row.dart
@@ -17,7 +17,7 @@ class PartyRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: onPress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
