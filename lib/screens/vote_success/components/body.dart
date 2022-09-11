f0d5fa10f00ce8d3373683188b47b879f39e73aa
AEZakmee <kristiyan99@protonmail.com>
2022-09-11 20:00:09 +0300

    Change success screen

--- lib/screens/vote_success/components/body.dart
+++ lib/screens/vote_success/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mashinno_glasuvane/model/data.dart';
 import 'package:mashinno_glasuvane/size_config.dart';
@@ -15,14 +16,20 @@ class Body extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Text(
-                'Вие гласувахте успешно!',
-                style: TextStyle(
-                  fontSize: getProportionateScreenWidth(25),
-                  fontWeight: FontWeight.bold,
-                ),
-                maxLines: 1,
+            Spacer(),
+            Icon(
+              Icons.check_circle,
+              color: Colors.lightBlue,
+              size: getProportionateScreenHeight(150),
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(30),
+            ),
+            Text(
+              "Разписката е готова",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(26),
+                fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(
@@ -51,6 +58,7 @@ class Body extends StatelessWidget {
               size: getProportionateScreenWidth(200),
               verticalSize: getProportionateScreenHeight(50),
             ),
+            Spacer(),
           ],
         ),
       ),
